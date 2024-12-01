819fe9558065793638227e0f4262e2d08a74fc07
Jashan-Windows <jashanbhullar376@gmail.com>
2024-12-01 20:02:09 +0530

    feat: load data for the selected hero

--- app.js
+++ app.js
@@ -78,9 +78,143 @@ searchList.addEventListener('click', (event) => {
     const singleHero = allData.filter(dataItem => {
         return dataItem.id === searchId;
     })[0];
+    searchList.innerHTML = "";
     showSuperheroDetails(singleHero);
 });
 
 const showSuperheroDetails = (heroData) => {
-    
+    const heroName = document.querySelector('.app-body-content-list .name');
+    const thumbnail = document.querySelector('.app-body-content-thumbnail');
+
+    heroName.innerHTML = heroData.name;
+    thumbnail.innerHTML = `
+    <img src = "${heroData.image.url}">
+    `;
+
+    const powerstats = document.querySelector('.powerstats');
+    const biography = document.querySelector('.biography');
+    const appearance = document.querySelector('.appearance');
+    const connections = document.querySelector('.connections');
+
+    powerstats.innerHTML = `
+        <li>
+            <div>
+                <i class = "fa-solid fa-shield-halved"></i>
+                <span>intelligence</span>
+            </div>
+            <span>${heroData.powerstats.intelligence}</span>
+        </li>
+        <li>
+            <div>
+                <i class = "fa-solid fa-shield-halved"></i>
+                <span>strength</span>
+            </div>
+            <span>${heroData.powerstats.strength}</span>
+        </li>
+        <li>
+            <div>
+                <i class = "fa-solid fa-shield-halved"></i>
+                <span>speed</span>
+            </div>
+            <span>${heroData.powerstats.speed}</span>
+        </li>
+        <li>
+            <div>
+                <i class = "fa-solid fa-shield-halved"></i>
+                <span>durability</span>
+            </div>
+            <span>${heroData.powerstats.durability}</span>
+        </li>
+        <li>
+            <div>
+                <i class = "fa-solid fa-shield-halved"></i>
+                <span>power</span>
+            </div>
+            <span>${heroData.powerstats.power}</span>
+        </li>
+        <li>
+            <div>
+                <i class = "fa-solid fa-shield-halved"></i>
+                <span>combat</span>
+            </div>
+            <span>${heroData.powerstats.combat}</span>
+        </li>
+    `;
+
+    biography.innerHTML = `
+        <li>
+            <span>full name</span>
+            <span>${heroData.biography['full-name']}</span>
+        </li>
+        <li>
+            <span>alert-egos</span>
+            <span>${heroData.biography['alter-egos'] ? heroData.biography['alter-egos'] : "no alter egos found."}</span>
+        </li>
+        <li>
+            <span>aliases</span>
+            <span>${heroData.biography.aliases.join(', ')}</span>
+        </li>
+        <li>
+            <span>place-of-birth</span>
+            <span>${heroData.biography['place-of-birth']}</span>
+        </li>
+        <li>
+            <span>first-apperance</span>
+            <span>${heroData.biography['first-appearance']}</span>
+        </li>
+        <li>
+            <span>publisher</span>
+            <span>${heroData.biography.publisher}</span>
+        </li>
+    `;
+
+    appearance.innerHTML = `
+        <li>
+            <span>
+                <i class = "fas fa-star"></i> gender
+            </span>
+            <span>${heroData.appearance.gender}</span>
+        </li>
+        <li>
+            <span>
+                <i class = "fas fa-star"></i> race
+            </span>
+            <span>${heroData.appearance.race}</span>
+        </li>
+        <li>
+            <span>
+                <i class = "fas fa-star"></i> height
+            </span>
+            <span>${heroData.appearance.height[0]}</span>
+        </li>
+        <li>
+            <span>
+                <i class = "fas fa-star"></i> weight
+            </span>
+            <span>${heroData.appearance.weight[0]}</span>
+        </li>
+        <li>
+            <span>
+                <i class = "fas fa-star"></i> eye-color
+            </span>
+            <span>${heroData.appearance['eye-color']}</span>
+        </li>
+        <li>
+            <span>
+                <i class = "fas fa-star"></i> hair-color
+            </span>
+            <span>${heroData.appearance['hair-color']}</span>
+        </li>
+    `;
+
+    connections.innerHTML = `
+        <li>
+            <span>group--affiliation</span>
+            <span>${heroData.connections['group-affiliation']}</span>
+        </li>
+        <li>
+            <span>relatives</span>
+            <span>${heroData.connections.relatives}</span>
+        </li>
+    `;
 };
